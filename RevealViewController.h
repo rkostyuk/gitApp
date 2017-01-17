fc193cfec0c7f9e0ffa08a5a56b013c356f659aa
Roman Kostyuk <kostyuk.roman@gmail.com>
2017-01-17 11:39:43 +0500

    Add reveal controller

--- RevealViewController.h
+++ RevealViewController.h
@@ -1,15 +1,13 @@
 //
-//  APIClient.h
+//  RevealViewController.h
 //  gitApp
 //
 //  Created by Roman Kostyuk on 1/16/17.
 //  Copyright © 2017 Roman Kostyuk. All rights reserved.
 //
 
-#import "AFHTTPSessionManager.h"
+#import <UIKit/UIKit.h>
 
-@interface APIClient : AFHTTPSessionManager
-
-+ (instancetype)sharedClient;
+@interface RevealViewController : UIViewController
 
 @end
---
+++ RevealViewController.m
@@ -0,0 +1,37 @@
+//
+//  RevealViewController.m
+//  gitApp
+//
+//  Created by Roman Kostyuk on 1/16/17.
+//  Copyright © 2017 Roman Kostyuk. All rights reserved.
+//
+
+#import "RevealViewController.h"
+
+@interface RevealViewController ()
+
+@end
+
+@implementation RevealViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"REVEAL";
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
