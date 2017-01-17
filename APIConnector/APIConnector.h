1a4f358df37e1f0ad2ec911c8f8c4f221ce51d1d
Roman Kostyuk <kostyuk.roman@gmail.com>
2017-01-17 17:50:22 +0500

    Remove API connector

--- APIConnector/APIConnector.h
+++
@@ -1,8 +0,0 @@
-//
-//  APIConnector.h
-//  allotaxi
-//
-//  Created by Roman Kostyuk on 5/24/15.
-//  Copyright (c) 2015 Roman Kostyuk. All rights reserved.
-//
-
--- APIConnector/APIConnector.m
+++
@@ -1,8 +0,0 @@
-//
-//  APIConnector.m
-//  allotaxi
-//
-//  Created by Roman Kostyuk on 5/24/15.
-//  Copyright (c) 2015 Roman Kostyuk. All rights reserved.
-//
-
