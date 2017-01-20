634b0433aa791f3695f87469ab6b6d8615f4d1e0
Roman Kostyuk <kostyuk.roman@gmail.com>
2017-01-20 13:41:03 +0500

    implement views

---
+++ PullRequestsViewController.m
@@ -0,0 +1,36 @@
+//
+//  PullRequestsViewController.m
+//  gitApp
+//
+//  Created by Roman Kostyuk on 1/19/17.
+//  Copyright © 2017 Roman Kostyuk. All rights reserved.
+//
+
+#import "PullRequestsViewController.h"
+
+@interface PullRequestsViewController ()
+
+@end
+
+@implementation PullRequestsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"Pull Requests";
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
